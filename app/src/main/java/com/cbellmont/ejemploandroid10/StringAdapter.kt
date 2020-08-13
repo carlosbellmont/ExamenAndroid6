2e733bdae776487c18591efd296bea67606e13ea
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-13 15:35:40 +0200

    Initial commit

---
+++ app/src/main/java/com/cbellmont/ejemploandroid10/StringAdapter.kt
@@ -0,0 +1,27 @@
+package com.cbellmont.ejemploandroid10
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class StringAdapter(var stringList : List<String>) : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
+
+    class StringViewHolder(root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        val twTextView = view.findViewById<TextView>(R.id.textView)
+        return StringViewHolder(view, twTextView)
+    }
+
+    override fun getItemCount(): Int {
+        return stringList.size
+    }
+
+    override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
+        holder.textView.text = stringList[position]
+    }
+}
+
